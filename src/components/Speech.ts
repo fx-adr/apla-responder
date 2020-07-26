38f9d1ebf12def13231dff3e48a6bf89dc0f21f8
Adam Rann <adam@fxdigital.uk>
2020-07-26 23:19:14 +0100

    feat(components): Speech component

---
+++ src/components/Speech.ts
@@ -0,0 +1,38 @@
+class Speech extends AbstractComponent {
+  type: string = "Speech";
+  private content: String = "";
+  private contentType: String = "PlainText";
+
+  private contentTypes: String[] = ["PlainText", "SSML"];
+
+  /**
+   * Add Alexa speech to speak in the response.
+   * @param content What to say
+   * @param contentType What format it's in.
+   */
+  constructor(content: String, contentType: String = "PlainText") {
+    super();
+    this.content = content;
+    if (this.contentTypes.includes(contentType)) {
+      this.contentType = contentType;
+    } else {
+        throw new InvalidArgumentError("Content type was not a valid argument.");
+    }
+  }
+
+  setContentType(contentType: String) {
+    if (this.contentTypes.includes(contentType)) {
+      this.contentType = contentType;
+    } else {
+        throw new InvalidArgumentError("Content type was not a valid argument.");
+    }
+  }
+
+  toObject() {
+    return {
+      type: this.type,
+      content: this.content,
+      contentType: this.contentType,
+    };
+  }
+}
