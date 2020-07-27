4e2c6f7bc57b5cc8a42d879b39efea6decd64ae7
Adam Rann <adam@fxdigital.uk>
2020-07-27 10:12:09 +0100

    feat(filters): FadeIn filter

---
+++ src/filter-classes/FadeIn.ts
@@ -0,0 +1,18 @@
+class FadeIn extends Filter {
+    type = "FadeIn"
+    duration: Number = 0;
+
+    constructor(duration:Number = 0) {
+        super();
+        if(duration < 0) {
+            throw new InvalidArgumentError("duration must be positive");
+        }
+        this.duration = duration;
+    }
+
+    toObject(): InflatedFilter {
+        const obj = super.toObject();
+        obj.duration = this.duration;
+        return obj;
+    }
+}
