58cba79c796868dd761ab7f1c9161b1897465af1
Adam Rann <adam@fxdigital.uk>
2020-07-27 12:15:55 +0100

    test: Ensure structure of FadeOut and FadeIn filters

--- src/filter-classes/FadeIn.test.ts
+++ src/filter-classes/FadeIn.test.ts
@@ -23,4 +23,41 @@ describe('FadeIn: Should set correct duration.', () => {
           new FadeIn(-1000);
         }).toThrowError(InvalidArgumentError);
     });
+
+    durations.forEach((duration) => {
+        test("Check structure  - " + duration + "ms", () => {
+          const fadeIn = new FadeIn(duration);
+          expect(fadeIn.toObject()).toEqual({
+              type: "FadeIn",
+              duration
+          });
+        });
+    });
+
+    test("When false", () => {
+        const duration = 362;
+        const fadeIn = new FadeIn(duration);
+        const when = false;
+        fadeIn.setWhen(when);
+        expect(fadeIn.toObject()).toEqual({
+              type: "FadeIn",
+              duration,
+              when
+          });
+    })
+
+    test("Description is kept", () => {
+      const description = "Fade in the sound.";
+      const duration = 433;
+      const fadeIn = new FadeIn(duration);
+      const when = false;
+      fadeIn.setWhen(when);
+      fadeIn.setDescription(description);
+      expect(fadeIn.toObject()).toEqual({
+        type: "FadeIn",
+        duration,
+        description,
+        when,
+      });
+    });
 });
--- src/filter-classes/FadeOut.test.ts
+++ src/filter-classes/FadeOut.test.ts
@@ -23,4 +23,41 @@ describe('FadeOut: Should set correct duration.', () => {
           new FadeOut(-1000);
         }).toThrowError(InvalidArgumentError);
     });
+
+    durations.forEach((duration) => {
+      test("Check structure  - " + duration + "ms", () => {
+        const fadeIn = new FadeOut(duration);
+        expect(fadeIn.toObject()).toEqual({
+          type: "FadeOut",
+          duration,
+        });
+      });
+    });
+
+    test("When false", () => {
+      const duration = 362;
+      const fadeIn = new FadeOut(duration);
+      const when = false;
+      fadeIn.setWhen(when);
+      expect(fadeIn.toObject()).toEqual({
+        type: "FadeOut",
+        duration,
+        when,
+      });
+    });
+
+    test("Description is kept", () => {
+      const description = "Fade in the sound.";
+      const duration = 433;
+      const fadeIn = new FadeOut(duration);
+      const when = false;
+      fadeIn.setWhen(when);
+      fadeIn.setDescription(description);
+      expect(fadeIn.toObject()).toEqual({
+        type: "FadeOut",
+        duration,
+        description,
+        when,
+      });
+    });
 });
