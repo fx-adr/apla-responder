f606c53ca242cbbeb39e761e2532ac6543ae8b73
Adam Rann <adam@fxdigital.uk>
2020-07-27 17:57:03 +0100

    test: Selector component test

---
+++ src/components/Selector.test.ts
@@ -0,0 +1,51 @@
+import Selector from "./Selector";
+import Audio from "./Audio";
+import Speech from "./Speech";
+
+const type = "Selector";
+
+describe("Silence component class", () => {
+  const selector = new Selector();
+
+  test("type is " + type, () => {
+    expect(selector.toObject().type).toEqual(type);
+  });
+
+  test("Adds item ok, and normal default strategy", () => {
+    const audio = new Audio("https://fxdigital.uk/");
+    const audioInflated = audio.toObject();
+
+    selector.addItem(audio);
+    expect(selector.toObject().items).toBeDefined();
+    expect(selector.toObject().items?.length).toEqual(1);
+    expect(selector.toObject()).toEqual({
+      type,
+      strategy: "normal",
+      items: [
+        audioInflated
+      ]
+    });
+  })
+
+  test("Handles multiple items", () => {
+    const selector = new Selector();
+
+    const audio = new Audio("https://fxdigital.uk/");
+    const speech = new Speech("hello there");
+    const audioInflated = audio.toObject();
+    const speechInflated = speech.toObject();
+
+    selector.addItem(audio);
+    selector.addItem(speech);
+
+    expect(selector.toObject().items).toBeDefined();
+    expect(selector.toObject().items?.length).toEqual(2);
+    expect(selector.toObject()).toEqual({
+      type,
+      strategy: "normal",
+      items: [audioInflated, speechInflated],
+    });
+  });
+
+
+});
