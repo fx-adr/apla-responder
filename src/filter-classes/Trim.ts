4a23c193decc8a470158699427b50b18dacc3ce4
Adam Rann <adam@fxdigital.uk>
2020-07-27 10:01:21 +0100

    feat(filters): Trim filter

---
+++ src/filter-classes/Trim.ts
@@ -0,0 +1,20 @@
+class Trim extends Filter {
+    type = "Trim"
+
+    start: Number = 0;
+    end: Number|null = null;
+
+    constructor(start:Number = 0, end: Number|null = null) {
+        super();
+        this.start = start;
+        this.end = end;
+    }
+
+    toObject(): InflatedFilter {
+        const obj = super.toObject();
+        obj.start = this.start;
+        obj.end = this.end;
+
+        return obj;
+    }
+}
---
+++ src/filter-classes/abstract-filter.ts
@@ -0,0 +1,26 @@
+abstract class Filter {
+  protected abstract type: String;
+  protected description: String = "";
+  protected when : String | true | false = true;
+
+  setDescription(description: String) {
+    this.description = description;
+  }
+
+  setWhen(when: String|true|false) {
+      this.when = when;
+  }
+
+  toObject(): InflatedFilter {
+    const obj : InflatedFilter = {
+        type: this.type,
+        when: this.when
+    }
+
+    if(this.description) {
+        obj.description = this.description;
+    }
+
+    return obj;
+  }
+}
